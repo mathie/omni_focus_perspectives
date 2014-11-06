32bcfbf56424a2cea75cf11c5b808943f8f93a5b
Graeme Mathieson <mathie@woss.name>
2014-11-06 11:31:36 +0000

    A couple more feature specs to demonstrate it works.

--- spec/features/perspectives_spec.rb
+++ spec/features/perspectives_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.feature 'Perspective management' do
   feature 'creating a new perspective' do
+    scenario 'I can find the new perspective link' do
+      visit '/perspectives'
+      
+      click_link 'New Perspective'
+      
+      expect(page).to have_content('Create a new perspective')
+    end
+    
     scenario 'User creates a new perspective' do
       visit '/perspectives/new'
     
@@ -31,10 +39,19 @@ RSpec.feature 'Perspective management' do
   end
   
   feature 'listing all perspectives' do
+    fixtures :perspectives
+
     scenario 'I can get to a list of perspectives from the home page' do
       visit '/'
       
       click_link 'All Perspectives'
+    end
+    
+    scenario 'I can see a list of all perspectives' do
+      visit '/perspectives'
+      
+      expect(page).to have_content('Today')
+      expect(page).to have_content('Plan')
     end
   end
 end
