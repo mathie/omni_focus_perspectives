15018818307ce6c2884b7d3802a0af3e1b740e9e
Graeme Mathieson <mathie@woss.name>
2014-11-12 15:29:45 +0000

    Make sure the code runs as well as passing tests...

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ worker_processes Integer(ENV['WEB_CONCURRENCY'] || 3)
 timeout 15
 preload_app true
 
-before_fork do
+before_fork do |_server, _worker|
   Signal.trap 'TERM' do
     puts 'Unicorn master intercepting TERM signal and sending itself QUIT instead.'
     Process.kill 'QUIT', Process.pid
@@ -11,7 +11,7 @@ before_fork do
   ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do
+after_fork do |_server, _worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM signal and ignoring it. Waiting on master sending a QUIT signal instead.'
   end
